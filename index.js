9add40ef4863e16028c9bd9b21132a014dc4d474
KASHIF-SHOUKAT-SE <149269224+KASHIF-SHOUKAT-SE@users.noreply.github.com>
2024-04-29 12:56:26 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+// let ctitle = document.getElementsById("firstcard")
+// ctitle.style.cololr = "red"
+// document.write(ctitle)
+
+
+let ctitiles = document.querySelectorAll(".cart-title")
+ctitiles[0].style.cololr = "blue"
+ctitiles[1].style.cololr = "red"
+ctitiles[2].style.cololr = "green"
+console.log(ctitiles)
